40b936d36e504d0b2d5ae3f26bc40a84c8cf1cc5
Pauloo <pauls2odins@gmail.com>
2023-03-23 00:31:00 +0200

    Logged In/Logged Out Windows

--- v2.1/main.py
+++ v2.1/main.py
@@ -10,6 +10,7 @@ from kivy.properties import ObjectProperty
 from kivy.graphics import Rectangle, RoundedRectangle
 from kivy.uix.widget import Widget
 from kivy_garden.mapview import MapMarkerPopup, MapView
+from kivy.uix.screenmanager import NoTransition
 
 class Map(MapView):
     def __init__(self, **kwargs):
@@ -21,19 +22,34 @@ class Map(MapView):
                 m.add_widget(Button(text=notikums[0]))
                 self.add_marker(m)
 
-class Home(GridLayout, Screen):
+class LOHome(GridLayout, Screen):
     pass
 
-class Notikumi(GridLayout, Screen):
+class LONotikumi(GridLayout, Screen):
     pass
 
-class Veikals(GridLayout, Screen):
+class LOVeikals(GridLayout, Screen):
     pass
 
-class FAQ(GridLayout, Screen):
+class LOFAQ(GridLayout, Screen):
     pass
 
-class ParMums(GridLayout, Screen):
+class LOParMums(GridLayout, Screen):
+    pass
+
+class LIHome(GridLayout, Screen):
+    pass
+
+class LINotikumi(GridLayout, Screen):
+    pass
+
+class LIVeikals(GridLayout, Screen):
+    pass
+
+class LIFAQ(GridLayout, Screen):
+    pass
+
+class LIParMums(GridLayout, Screen):
     pass
 
 class Pieslegties(GridLayout, Screen):
@@ -41,15 +57,22 @@ class Pieslegties(GridLayout, Screen):
     pwd = ObjectProperty(None)
 
     def login(self):
+        global user_logon_email
+        global user_logon_id
+        global user_logon_user
         if self.email.text and self.pwd.text:
             with db.connect("sakoplatviju.db") as con:
                 iev_email = self.email.text
                 iev_pwd = self.pwd.text
-                atrasta = con.execute("""SELECT parole_hash FROM Lietotaji WHERE epasts=?""", (iev_email,)).fetchall()
+                atrasta = con.execute("""SELECT id, parole_hash FROM Lietotaji WHERE epasts=?""", (iev_email,)).fetchall()
                 if atrasta:
-                    if atrasta[0][0].strip(",") == iev_pwd:
-                        #ieiet apliakcija
+                    print(atrasta)
+                    if atrasta[0][1] == iev_pwd:
                         self.noti.text = "Veiksmīga pieslēgšanās!"
+                        sm.current = "lihome"
+                        user_logon_id = atrasta[0][0]
+                        user_logon_user = atrasta[0][1]
+                        user_logon_email = iev_email
                     else:
                         self.noti.text = "Nepareiza parole!"
                 else:
@@ -85,17 +108,46 @@ class Registreties(GridLayout, Screen):
         else:
             self.noti.text = "Lūdzu aizpildiet visus laukus!"
 
+class Profils(GridLayout, Screen):
+    user = ObjectProperty(None)
+    cash = ObjectProperty(None)
+
+    def update_label(self):
+        try:
+            self.user.text = user_logon_user
+            print(user_logon_user)
+        except:
+            pass
+
 class WindowManager(ScreenManager):
     pass
 
 kivy.require('1.9.1')
 
 kvfile = Builder.load_file("main.kv")
+sm = WindowManager(transition=NoTransition())
+
+sm.add_widget(LOHome(name="lohome"))
+sm.add_widget(LONotikumi(name="lonotikumi"))
+sm.add_widget(LOVeikals(name="loveikals"))
+sm.add_widget(LOFAQ(name="lofaq"))
+sm.add_widget(LOParMums(name="loparmums"))
+sm.add_widget(Pieslegties(name="pieslegties"))
+sm.add_widget(Registreties(name="registreties"))
+sm.add_widget(LIHome(name="lihome"))
+sm.add_widget(LINotikumi(name="linotikumi"))
+sm.add_widget(LIVeikals(name="liveikals"))
+sm.add_widget(LIFAQ(name="lifaq"))
+sm.add_widget(LIParMums(name="liparmums"))
+sm.add_widget(Profils(name="profils"))
+
+user_logon_user = None
+user_logon_email = None
+user_logon_id = None
 
 class main2(App):
-
     def build(self):
-        return kvfile
+        return sm
 
 if __name__ == "__main__":
     kv = main2()
